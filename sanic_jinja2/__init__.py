b6a1131f29b761296234efd2177791f8f36b89eb
lixxu <xuzenglin@gmail.com>
2017-03-09 22:33:19 +0800

    add render_source function, and update context for babel

--- sanic_jinja2/__init__.py
+++ sanic_jinja2/__init__.py
@@ -4,11 +4,13 @@
 from functools import partial
 from sanic.response import html
 from jinja2 import Environment, PackageLoader
+from jinja2.ext import _make_new_gettext, _make_new_ngettext
 
 
 class SanicJinja2:
     def __init__(self, app=None, loader=None, **kwargs):
         self.env = Environment(**kwargs)
+        self.app = app
         if app:
             self.init_app(app, loader)
 
@@ -19,6 +21,7 @@ class SanicJinja2:
             self.env.filters[name] = obj
 
     def init_app(self, app, loader=None):
+        self.app = app
         if not hasattr(app, 'extensions'):
             app.extensions = {}
 
@@ -30,7 +33,7 @@ class SanicJinja2:
         self.env.loader = loader
         self.add_env('app', app)
         self.add_env('url_for', app.url_for)
-        self.add_env('_', self.fake_trans)
+        self.url_for = app.url_for
 
         @app.middleware('request')
         async def add_flash_to_request(request):
@@ -41,6 +44,15 @@ class SanicJinja2:
         return text
 
     def update_request_context(self, request, context):
+        if 'babel' in request.app.extensions:
+            babel = request.app.babel_instance
+            g = _make_new_gettext(babel._get_translations(request).ugettext)
+            ng = _make_new_ngettext(babel._get_translations(request).ungettext)
+            context.setdefault('gettext', g)
+            context.setdefault('ngettext', ng)
+            context.setdefault('_', context['gettext'])
+
+        context.setdefault('_', self.fake_trans)
         context.setdefault('request', request)
         context.setdefault('get_flashed_messages',
                            partial(self._get_flashed_messages, request))
@@ -52,6 +64,10 @@ class SanicJinja2:
     async def render_async(self, template, request, **context):
         return html(await self.render_string_async(template, request,
                                                    **context))
+
+    def render_source(self, source, request, **context):
+        self.update_request_context(request, context)
+        return self.env.from_string(source).render(**context)
 
     def render_string(self, template, request, **context):
         self.update_request_context(request, context)
